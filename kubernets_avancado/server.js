3a750ead9f6a5b6eda3e8d426a3a1d6a493bbeaa
Nailson <nailson.ivs@codens.com.br>
2025-01-12 16:41:41 -0300

    :tada: First commit

---
+++ kubernets_avancado/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+const startDate = new Date();
+
+app.get('/', (req, res) => {
+    res.send('Treina Web DevOps')
+})
+
+app.get('/healthz', (req, res) => {
+    var requestDate = new Date();
+    var diff = Math.abs(startDate.getTime() - requestDate.getTime())
+    var seconds = diff / 1000;
+
+    if (seconds < 10 || seconds > 50){
+        res.sendStatus(500);
+    }else{
+        res.sendStatus(200);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Acesse em http://localhost:${port}`)
+})
