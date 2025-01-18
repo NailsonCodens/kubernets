50e0740ec59caead25d2604d0a2af3510e3e3bd7
Nailson <nailson.ivs@codens.com.br>
2025-01-18 13:29:52 -0300

    ít is the end

---
+++ kubernets_avancado_2/server.js
@@ -0,0 +1,14 @@
+require("dotenv/config")
+const express = require("express");
+
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("UserName " + process.env.USER + " executando em: " + process.env.PASSWORD);
+})
+
+app.listen(port, () => {
+    console.log(`Acesse em http://localhost:${port}`)
+})
